60af5166348c1195fcca5921efa7c931d56952ea
raxitcholera <raxitcholera@gmail.com>
2017-09-03 11:35:59 -0700

    feat: Added the play functionality
    Now the recorded affirmations can be played if the pre-populated
    affirmations have been recorded once.
    
    They can be edited by selecting the edit option in the tableview.

--- MyAffirmations/PlayAudioViewController.swift
+++ MyAffirmations/PlayAudioViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayAudioViewController: UIViewController {
     
     var selectedAffermation:Affermations?
+    
+    @IBOutlet weak var AffermationName: UILabel!
+    @IBOutlet weak var PlayButton: UIButton!
+    @IBOutlet weak var StopButton: UIButton!
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        AffermationName.text = selectedAffermation?.name
 
         // Do any additional setup after loading the view.
     }
@@ -23,15 +30,53 @@ class PlayAudioViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func PlayButtonPressed(_ sender:UIButton){
+        playAudio()
     }
-    */
+    
+    @IBAction func stopButtonPressed(_ sender:UIButton){
+        stopAudio()
+    }
 
+}
+
+
+extension PlayAudioViewController: AVAudioPlayerDelegate {
+
+    enum PlayingState { case playing, notPlaying }
+    
+    func stopAudio() {
+        audioPlayer.stop()
+        configureUI(PlayingState.notPlaying)
+    }
+    func playAudio() {
+        
+        // initialize audio engine components
+        let data = selectedAffermation!.audiofile
+        do {
+            audioPlayer = try AVAudioPlayer(data: data! as Data)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+            configureUI(PlayingState.playing)
+            
+        } catch{
+            //error message
+            configureUI(PlayingState.notPlaying)
+        }
+    }
+    
+    func configureUI(_ playState: PlayingState) {
+        switch(playState) {
+        case .playing:
+            StopButton.isHidden = false
+            PlayButton.isHidden = true
+            
+        case .notPlaying:
+            StopButton.isHidden = true
+            PlayButton.isHidden = false
+            
+        }
+    }
+    
+    
 }
